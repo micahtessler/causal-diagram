efcb121095f9d6a3d9e2c7603e10a547762553fd
Tessler <MTESSLER@ford.com>
2018-12-07 17:13:12 -0500

    More of the SVG drawing code migrated

--- src/app/diagram.service.spec.ts
+++ src/app/diagram.service.spec.ts
@@ -20,161 +20,143 @@ describe('DiagramService', () => {
   });
 
   it('should be created', () => {
-   
+
     expect(service).toBeTruthy();
   });
 
-  describe("beatDiff", function() {
-		let throwObj: Throw;
+  describe("beatDiff", function () {
+    let throwObj: Throw;
 
-		beforeEach(function() {
-			throwObj = {
-					sourceJuggler:0,
-					sourceHand:"R",
-					targetJuggler:0,
-					throwHeight: 3
-			};
+    beforeEach(function () {
+      throwObj = {
+        sourceJuggler: 0,
+        sourceHand: "R",
+        targetJuggler: 0,
+        throwHeight: 3
+      };
     });
-    
-    describe("drawSVG", function() {
-			let pattern: Pattern;
-			let totalBeats: number;
-			let totalJugglers: number;
-			
-			let circlesDrawn;
-			let throwsDrawn;
-			let defsWithArrow;
-			
-			beforeEach(function() {
-				
-				totalBeats = 3;
-				totalJugglers = 3;
-				circlesDrawn = [];
-				throwsDrawn = [];
-				defsWithArrow = [];
-				pattern = {
-						name : "Test",
-						jugglerCount : totalJugglers,
-						beats : []
-				};
-				for (let i = 0; i < totalBeats; i++) {
-					let beat = {
-						throws : []
-					};
-					pattern.beats.push(beat);
-					for (let j = 0; j < totalJugglers; j++) {
-						let throwObj = {
-							"sourceJuggler":j,
-							"sourceHand":"R",
-							"targetJuggler":j,
-							"throwHeight": 3
-						};
-						beat.throws.push(throwObj);
-					}
-				}
+
+    describe("drawSVG", function () {
+      let pattern: Pattern;
+      let totalBeats: number;
+      let totalJugglers: number;
+
+
+      beforeEach(function () {
+
+        totalBeats = 3;
+        totalJugglers = 3;
+
+        pattern = {
+          name: "Test",
+          jugglerCount: totalJugglers,
+          beats: []
+        };
+        for (let i = 0; i < totalBeats; i++) {
+          let beat = {
+            throws: []
+          };
+          pattern.beats.push(beat);
+          for (let j = 0; j < totalJugglers; j++) {
+            let throwObj = {
+              "sourceJuggler": j,
+              "sourceHand": "R",
+              "targetJuggler": j,
+              "throwHeight": 3
+            };
+            beat.throws.push(throwObj);
+          }
+        }
         patternService.selectedPattern = pattern;
 
-				//clear the page...
-				div = document.createElement("div");
-				draw = null;
-				service.drawCircles = function(draw, beatNb, throws) {
-					circlesDrawn[beatNb]={
-						"draw": draw,
-						"beatNb": beatNb,
-						"throws": throws
-					};
-				}
-				service.drawThrows = function(draw, beatNb, throws, arrowMarker) {
-					throwsDrawn[beatNb] ={
-						"draw": draw,
-						"beatNb": beatNb,
-						"throws": throws,
-						"arrowMarker": arrowMarker
-					};
-				}
-				service.initArrow = function(defs) {
-					defsWithArrow = defs;
-					return {
-						"id":"fooArrow"
-					};
-				}
-			});
-			
-			it("should draw the diagram correctly", function() {
-				draw = service.drawSVG(div);
+        //clear the page...
+        div = document.createElement("div");
+        draw = null;
+        spyOn(service, 'drawCircles');
+        spyOn(service, 'drawThrows');
+        spyOn(service, 'initArrow').and.returnValue(
+          {
+            "id": "fooArrow"
+          }
+        );
+      });
+
+      it("should draw the diagram correctly", function () {
+        draw = service.drawSVG(div);
         console.log('div', div, Object.keys(div));
 
-				let mySvg = 	div.firstChild as SVGElement;
+        let mySvg = div.firstChild as SVGElement;
 
         let viewWidth = service.BEAT_WIDTH * totalBeats;
-				let viewHeight = service.BEAT_HEIGHT * (totalJugglers + 1);
+        let viewHeight = service.BEAT_HEIGHT * (totalJugglers + 1);
 
-        expect(mySvg.getAttribute('viewBox')).toEqual("0 0 "+viewWidth+" "+viewHeight);
-				/*
-				let defs = mySvg.childNodes[0];
-				expect(defs.tagName).toEqual("defs");
-				expect(defsWithArrow).toBe(defs.instance);
-				
-				expect(circlesDrawn.length).toEqual(totalBeats);
-				for (let i = 0; i < totalBeats; i++) {
-					expect(circlesDrawn[i].draw).toBe(draw);
-					expect(circlesDrawn[i].beatNb).toEqual(i);
-					expect(circlesDrawn[i].throws).toBe(pattern.beats[i].throws);
-				}
-					
-				expect(throwsDrawn.length).toEqual(totalBeats);
-				for (let i = 0; i < totalBeats; i++) {
-					expect(throwsDrawn[i].draw).toBe(draw);
-					expect(throwsDrawn[i].beatNb).toEqual(i);
-					expect(throwsDrawn[i].throws).toBe(pattern.beats[i].throws);
-					expect(throwsDrawn[i].arrowMarker.id).toEqual("fooArrow");
+        expect(mySvg.getAttribute('viewBox')).toEqual("0 0 " + viewWidth + " " + viewHeight);
+
+        expect(service.drawCircles['calls'].count()).toEqual(totalBeats);
+        for (let i = 0; i < totalBeats; i++) {
+          expect(service.drawCircles['calls'].argsFor(i)).toEqual(
+            [
+              draw, i, pattern.beats[i].throws
+            ]
+          );
+
         }
-        */
-      });
-		});
+        expect(service.drawThrows['calls'].count()).toEqual(totalBeats);
+        for (let i = 0; i < totalBeats; i++) {
+          expect(service.drawThrows['calls'].argsFor(i).slice(0, 4)).toEqual(
+            [
+              draw, i, pattern.beats[i].throws,
+              { "id": "fooArrow" }
+            ]
+          );
 
-		it("should calculate beat diff", function() {
-			for (let i = 0; i < 20; i++) {
-				throwObj.throwHeight = i;
-				let diff = service.getBeatDiff(throwObj);
-				expect(diff).toEqual(i - 2);
-			}
-		});
-	});
+        }
+
+      });
+    });
+
+    it("should calculate beat diff", function () {
+      for (let i = 0; i < 20; i++) {
+        throwObj.throwHeight = i;
+        let diff = service.getBeatDiff(throwObj);
+        expect(diff).toEqual(i - 2);
+      }
+    });
+  });
 
   describe("getBeatPosition", () => {
     let circle_corner_adj;
-		let totalBeats;
-		let totalJugglers;
-    beforeEach(function() {
-			totalBeats = 20;
-			totalJugglers = 20;
-			circle_corner_adj = (Math.round((Math.cos(service.CIRCLE_CORNER_ANGLE)*service.CIRCLE_RADIUS)*100) / 100);
-		});
-		it("should calculate beat position", () => {
-			for (let beat = 0; beat < 20; beat++) {
-				for (let juggler = 0; juggler < 20; juggler++) {
-					let pos = service.getBeatPosition(beat, juggler);
-					expect(pos.x).toEqual(beat * 100 + 50);
-					expect(pos.y).toEqual(juggler * 125 + 75);
-				}
-			}
+    let totalBeats;
+    let totalJugglers;
+    beforeEach(function () {
+      totalBeats = 20;
+      totalJugglers = 20;
+      circle_corner_adj = (Math.round((Math.cos(service.CIRCLE_CORNER_ANGLE) * service.CIRCLE_RADIUS) * 100) / 100);
     });
-    
-    describe('Circle edge positions', ()=> {
-      it("should  calculate upper left", function() {
+    it("should calculate beat position", () => {
+      for (let beat = 0; beat < 20; beat++) {
+        for (let juggler = 0; juggler < 20; juggler++) {
+          let pos = service.getBeatPosition(beat, juggler);
+          expect(pos.x).toEqual(beat * 100 + 50);
+          expect(pos.y).toEqual(juggler * 125 + 75);
+        }
+      }
+    });
+
+    describe('Circle edge positions', () => {
+      it("should  calculate upper left", function () {
         for (let beat = 0; beat < totalBeats; beat++) {
           for (let juggler = 0; juggler < totalJugglers; juggler++) {
             let pos = service.getBeatPosition(beat, juggler);
             let posX = service.getCircleUpperLeft(pos);
-            
+
             expect(posX.x).toEqual(beat * 100 + 50 - circle_corner_adj);
             expect(posX.y).toEqual(juggler * 125 + 75 - circle_corner_adj);
           }
         }
       });
-  
-      it("should  calculate upper right", function() {
+
+      it("should  calculate upper right", function () {
         for (let beat = 0; beat < totalBeats; beat++) {
           for (let juggler = 0; juggler < totalJugglers; juggler++) {
             let pos = service.getBeatPosition(beat, juggler);
@@ -184,8 +166,8 @@ describe('DiagramService', () => {
           }
         }
       });
-  
-      it("should  calculate lower left", function() {
+
+      it("should  calculate lower left", function () {
         for (let beat = 0; beat < totalBeats; beat++) {
           for (let juggler = 0; juggler < totalJugglers; juggler++) {
             let pos = service.getBeatPosition(beat, juggler);
@@ -195,8 +177,8 @@ describe('DiagramService', () => {
           }
         }
       });
-  
-      it("should  calculate lower right", function() {
+
+      it("should  calculate lower right", function () {
         for (let beat = 0; beat < totalBeats; beat++) {
           for (let juggler = 0; juggler < totalJugglers; juggler++) {
             let pos = service.getBeatPosition(beat, juggler);
@@ -206,8 +188,8 @@ describe('DiagramService', () => {
           }
         }
       });
-  
-      it("should  calculate right", function() {
+
+      it("should  calculate right", function () {
         for (let beat = 0; beat < totalBeats; beat++) {
           for (let juggler = 0; juggler < totalJugglers; juggler++) {
             let pos = service.getBeatPosition(beat, juggler);
@@ -217,8 +199,8 @@ describe('DiagramService', () => {
           }
         }
       });
-  
-      it("should  calculate left", function() {
+
+      it("should  calculate left", function () {
         for (let beat = 0; beat < totalBeats; beat++) {
           for (let juggler = 0; juggler < totalJugglers; juggler++) {
             let pos = service.getBeatPosition(beat, juggler);
@@ -228,8 +210,8 @@ describe('DiagramService', () => {
           }
         }
       });
-  
-      it("should  calculate top ", function() {
+
+      it("should  calculate top ", function () {
         for (let beat = 0; beat < totalBeats; beat++) {
           for (let juggler = 0; juggler < totalJugglers; juggler++) {
             let pos = service.getBeatPosition(beat, juggler);
@@ -239,8 +221,8 @@ describe('DiagramService', () => {
           }
         }
       });
-  
-      it("should  calculate bottom ", function() {
+
+      it("should  calculate bottom ", function () {
         for (let beat = 0; beat < totalBeats; beat++) {
           for (let juggler = 0; juggler < totalJugglers; juggler++) {
             let pos = service.getBeatPosition(beat, juggler);
@@ -252,5 +234,149 @@ describe('DiagramService', () => {
       });
     });
   });
-  
+
+  describe("drawCircles", function () {
+    let throws;
+    let totalBeats;
+    let totalJugglers;
+    let drawSpy;
+    let circleSpy;
+    let textSpy;
+    beforeEach(function () {
+      totalBeats = 3;
+      totalJugglers = 3;
+      throws = [];
+      for (let i = 0; i < totalJugglers; i++) {
+        const throwObj = {
+          "sourceJuggler": i,
+          "sourceHand": (i % 2 == 0) ? "R" : "L",
+          "targetJuggler": i,
+          "throwHeight": 3
+        };
+        throws.push(throwObj);
+      }
+      drawSpy = {};
+      circleSpy = {};
+      textSpy = {};
+      drawSpy.circle = jasmine.createSpy().and.returnValue(circleSpy);
+      drawSpy.text = jasmine.createSpy().and.returnValue(textSpy);
+      circleSpy.radius = jasmine.createSpy();
+      circleSpy.cx = jasmine.createSpy();
+      circleSpy.cy = jasmine.createSpy();
+      circleSpy.id = jasmine.createSpy();
+      circleSpy.addClass = jasmine.createSpy();
+      textSpy.x = jasmine.createSpy();
+      textSpy.y = jasmine.createSpy();
+      textSpy.id = jasmine.createSpy();
+      textSpy.addClass = jasmine.createSpy();
+      textSpy.font = jasmine.createSpy();
+    });
+
+    it("shouldDrawCirlesCorrectly", function () {
+      for (let beat = 0; beat < totalBeats; beat++) {
+        service.drawCircles(drawSpy, beat, throws);
+      }
+      expect(drawSpy.circle.calls.count()).toEqual(totalBeats * totalJugglers);
+      for (let beat = 0; beat < totalBeats; beat++) {
+        for (let juggler = 0; juggler < totalJugglers; juggler++) {
+          const pos = service.getBeatPosition(beat, juggler);
+          expect(circleSpy.radius.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual([service.CIRCLE_RADIUS]);
+          expect(circleSpy.cx.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual([pos.x]);
+          expect(circleSpy.cy.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual([pos.y]);
+          expect(circleSpy.id.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual(["causal_beat_" + beat + "_juggler_" + juggler]);
+          expect(circleSpy.addClass.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual(["causal_beat_circle"]);
+        }
+      }
+
+    });
+
+    it("shouldDrawTextCorrectly", function () {
+      for (let beat = 0; beat < totalBeats; beat++) {
+        service.drawCircles(drawSpy, beat, throws);
+      }
+      expect(drawSpy.text.calls.count()).toEqual(totalBeats * totalJugglers);
+
+      for (let beat = 0; beat < totalBeats; beat++) {
+        for (let juggler = 0; juggler < totalJugglers; juggler++) {
+          const pos = service.getBeatPosition(beat, juggler);
+          expect(drawSpy.text.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual([throws[juggler].sourceHand]);
+          expect(textSpy.x.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual([pos.x - 10]);
+          expect(textSpy.y.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual([pos.y - 30]);
+          expect(textSpy.id.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual(["causal_beat_label_" + beat + "_juggler_" + juggler]);
+          expect(textSpy.addClass.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual(["causal_beat_label"]);
+          expect(textSpy.font.calls.argsFor(beat * totalJugglers + juggler)).
+            toEqual([{
+              family: 'sans-serif'
+              , size: "35"
+            }]);
+
+        }
+      }
+    });
+
+  });
+
+  describe('drawThrows', () => {
+    let drawSpy, arrowMarker, throws, borderEast, borderWest;
+    beforeEach(() => {
+      drawSpy = {
+        id: "draw"
+      };
+      spyOn(service, 'drawThrowLine');
+      spyOn(service, 'drawSelfCurve');
+      spyOn(service, 'isLineInDiagram').and.callFake((throwObj) => {
+        return throwObj.sourceJuggler != throwObj.targetJuggler;
+      });
+
+      arrowMarker = {
+        id: 'foo'
+      };
+      borderEast = {
+        id: 'east'
+      };
+      borderWest = {
+        id: 'west'
+      };
+    });
+
+    it("should draw the right passes", () => {
+
+      throws = [];
+      for (let i = 0; i < 3; i++) {
+        const throwObj = {
+          "sourceJuggler": i,
+          "sourceHand": (i % 2 == 0) ? "R" : "L",
+          "targetJuggler": i,
+          "throwHeight": 3
+        };
+        throws.push(throwObj);
+      }
+      throws[0].targetJuggler=1;
+      throws[1].targetJuggler=0;
+      service.drawThrows(drawSpy, 6, throws, arrowMarker, borderEast, borderWest);
+      expect(service.isLineInDiagram['calls'].count()).toEqual(3);
+      for (let i = 0; i < 3; i++) {
+        expect(service.isLineInDiagram['calls'].argsFor(i)).toEqual([throws[i]]);
+      }
+      expect(service.drawThrowLine['calls'].count()).toEqual(2);
+      expect(service.drawThrowLine['calls'].argsFor(0)).
+      toEqual([drawSpy, "throw_6_0", throws[0], 6, arrowMarker, borderEast, borderWest]);
+      expect(service.drawThrowLine['calls'].argsFor(1)).
+      toEqual([drawSpy, "throw_6_1", throws[1], 6, arrowMarker, borderEast, borderWest]);
+      expect(service.drawSelfCurve['calls'].count()).toEqual(1);
+      expect(service.drawSelfCurve['calls'].argsFor(0)).
+      toEqual([drawSpy, "throw_6_2", throws[2], 6, arrowMarker, borderEast, borderWest]);
+      
+    });
+  });
 });
--- src/app/diagram.service.ts
+++ src/app/diagram.service.ts
@@ -22,8 +22,8 @@ export class DiagramService {
   readonly CIRCLE_CORNER_X = (Math.round((Math.cos(this.CIRCLE_CORNER_ANGLE) * this.CIRCLE_RADIUS) * 100) / 100);
   readonly CIRCLE_CORNER_Y = (Math.round((Math.sin(this.CIRCLE_CORNER_ANGLE) * this.CIRCLE_RADIUS) * 100) / 100);
 
-  drawSVG(div: HTMLDivElement): void {
-    
+  drawSVG(div: HTMLDivElement): SVG.Doc {
+
     const draw = SVG(div);
 
 
@@ -33,29 +33,38 @@ export class DiagramService {
     draw.viewbox(0, 0, diagramWidth, diagramHeight);
 
     //create arrow heads
-    var defs = draw.defs();
-    var arrowMarker = this.initArrow(defs);
+    const defs = draw.defs();
+    const arrowMarker = this.initArrow(defs);
 
 
-    var borderWest = draw.line(0, 0, 0, diagramHeight);
+    const borderWest = draw.line(0, 0, 0, diagramHeight);
     borderWest.addClass("causal_border");
     borderWest.id("causal_border_west");
 
 
-    var borderEast = draw.line(diagramWidth, 0, diagramWidth, diagramHeight);
+    const borderEast = draw.line(diagramWidth, 0, diagramWidth, diagramHeight);
     borderEast.addClass("causal_border");
     borderEast.id("causal_border_east");
 
 
-    var borderNorth = draw.line(0, 1, diagramWidth, 1);
+    const borderNorth = draw.line(0, 1, diagramWidth, 1);
     borderNorth.addClass("causal_border");
     borderNorth.id("causal_border_north");
 
 
-    var borderSouth = draw.line(0, diagramHeight, diagramWidth, diagramHeight);
+    const borderSouth = draw.line(0, diagramHeight, diagramWidth, diagramHeight);
     borderSouth.addClass("causal_border");
     borderSouth.id("causal_border_south");
 
+    this.patternService.selectedPattern.beats.forEach((beat, i) => {
+      this.drawCircles(draw, i, beat.throws);
+    });
+    this.patternService.selectedPattern.beats.forEach((beat, i) => {
+      this.drawThrows(draw, i, beat.throws, arrowMarker, borderEast, borderWest);
+    });
+
+
+    return draw;
   }
 
   getBeatDiff(throwObj: Throw): number {
@@ -118,14 +127,59 @@ export class DiagramService {
     );
   }
 
-  drawCircles(draw: any, beatNb: number, throws: Array<Throw>): void {
+  drawCircles(draw: SVG.Doc, beatNb: number, throws: Array<Throw>): void {
+    throws.forEach((throwObj) => {
+      const pos = this.getBeatPosition(beatNb, throwObj.sourceJuggler);
+      //console.log("Drawing throw "+j+" at "+beatX+","+beatY);
+      //create the circle
+      const circle = draw.circle();
 
+      circle.radius(this.CIRCLE_RADIUS);
+      circle.cx(pos.x);
+      circle.cy(pos.y);
+      circle.id("causal_beat_" + beatNb + "_juggler_" + throwObj.sourceJuggler);
+      circle.addClass("causal_beat_circle");
+
+      const text = draw.text(throwObj.sourceHand);
+      text.x(pos.x - 10);
+      text.y(pos.y - 30);
+      text.id("causal_beat_label_" + beatNb + "_juggler_" + throwObj.sourceJuggler);
+      text.addClass("causal_beat_label")
+      text.font({
+        family: 'sans-serif'
+        , size: "35"
+      });
+    });
   }
 
-  drawThrows(draw: any, beatNb: number, throws: Array<Throw>, arrowMarker: any): void {
+  drawThrows(draw: SVG.Doc, beatNb: number, throws: Array<Throw>, arrowMarker: any, borderEast: SVG.Line, borderWest: SVG.Line): void {
+    for (let j = 0; j < throws.length; j++) {
+      //TODO: need a better ID system for throws (multiplexes...)
+      const id = "throw_" + beatNb + "_" + throws[j].sourceJuggler;
 
+      if (this.isLineInDiagram(throws[j])) {
+        this.drawThrowLine(draw, id, throws[j], beatNb, arrowMarker, borderEast, borderWest);
+      } else {
+        //this is a curve for a self throw (not 1 or 3)
+        this.drawSelfCurve(draw, id, throws[j], beatNb, arrowMarker, borderEast, borderWest);
+      }
+    }
   }
   initArrow(defs: any): any {
+    //TODO: not implemented
     return null;
+  }
+
+  isLineInDiagram(throwObj: Throw): boolean {
+    //TODO: not implemented
+    return true;
+  }
+
+  drawThrowLine(draw: SVG.Doc, id: string, throwObj: Throw, beatNb: number, arrowMarker: any, borderEast: SVG.Line, borderWest: SVG.Line): void {
+    //TODO: not implemented
+  }
+  
+  drawSelfCurve(draw: SVG.Doc, id: string, throwObj: Throw, beatNb: number, arrowMarker: any, borderEast: SVG.Line, borderWest: SVG.Line): void {
+    //TODO: not implemented
   }
 }
