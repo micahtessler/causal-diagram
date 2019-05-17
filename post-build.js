b737a5e2c23f2e5510a8cad9157cf421c53a5a10
fcprxbci <fcprxbci@ford.com>
2019-05-17 17:07:53 -0400

    ANother try at github pages

--- post-build.js
+++ post-build.js
@@ -1,34 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const directoryPath = path.join(__dirname, 'dist', 'causal-diagram');
 const docsFolder = path.join(__dirname, 'docs');
 
-function handleErr(desc, err) {
-    if (err) {
-        console.err(desc, err);
-        throw err;
-    }
-}
+const index_html = path.join(docsFolder, 'index.html');
+const four04_html = path.join(docsFolder, '404.html');
 
-//Delete existing docs
-const docsExists = fs.existsSync(docsFolder);
-if (docsExists) {
-    const docFiles = fs.readdirSync(docsFolder);
-    docFiles.forEach((file) => {
-        const filePath = path.join(docsFolder, file);
-        fs.unlinkSync(filePath);
-    });
-    fs.rmdirSync(docsFolder);
-}
-
-
-fs.mkdirSync(docsFolder);
-
-const distFiles = fs.readdirSync(directoryPath);
-distFiles.forEach((file) => {
-    const filePath = path.join(directoryPath, file);
-    const data = fs.readFileSync(filePath);
-    const outputPath = path.join(docsFolder, file);
-    fs.writeFileSync(outputPath, data);
-});
+const data = fs.readFileSync(index_html);
+fs.writeFileSync(four04_html, data);
