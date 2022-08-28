df8734868192d490bac79375c74c103d4957e50f
Micah Tessler <micah_tessler@yahoo.com>
2022-08-28 02:10:43 +0000

    AUdit fix and move to eslint

--- src/app/diagram-card/diagram-card.component.ts
+++ src/app/diagram-card/diagram-card.component.ts
@@ -7,10 +7,8 @@ import { DiagramService } from '../diagram.service';
   templateUrl: './diagram-card.component.html',
   styleUrls: ['./diagram-card.component.css']
 })
-export class DiagramCardComponent implements OnInit {
+export class DiagramCardComponent {
   
   constructor(public patternService : PatternService) { }
 
-  ngOnInit() {
-  }
 }
